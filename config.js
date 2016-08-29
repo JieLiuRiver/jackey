7b73e2b0516ccc352578b06910f1d320f7bfd687
stupidWall <268079@163.com>
2016-08-29 13:06:06 +0800

    搭起简单框架

---
+++ config.js
@@ -0,0 +1,16 @@
+/*
+* 配置require相关内容、文件目录，加载模块等
+* author : heliujie
+* */
+require.config({
+   // baseUrl : ''
+    paths : {
+        'jquery' : "./public/libs/jquery/dist/jquery",
+        'bootstrap' : "./public/libs/bootstrap/dist/js/bootstrap.min",
+        "index" : "./public/js/index"
+    }
+});
+
+require(['jquery', 'bootstrap', 'index'], function(jquery, bootstrap, index){
+    index.init();
+});
---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>小蚁</title>
+    <!-- 公用css -->
+    <link rel="stylesheet" href="public/css/reset.css">
+    <link rel="stylesheet" href="public/css/common.css">
+    <link rel="stylesheet" href="public/libs/bootstrap/dist/css/bootstrap.css">
+
+    <!-- require.js -->
+    <script data-main="config" src="public/libs/require/require.js"></script>
+</head>
+<body>
+    
+</body>
+</html>
